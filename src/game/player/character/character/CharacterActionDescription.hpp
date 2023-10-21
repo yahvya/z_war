1e43e97a0104b307a80c25475f4218fceebc512e
Bathily Yahaya <developpeur.yb@gmail.com>
2023-10-21 02:33:09 +0200

    Ajout des structures de description du personnage (à vérifier / terminer) Ajout des fonctions de vérifications de chargement (à vérifier) CharacterAction à implémenter Ajout des fonctions de récupération partielles, récupération de la map d'action à faire (tests à continuer) Fonction de chargement des données d'animation à terminer Changement d'utilisations const char* -> std::string décommenter la vérification d'existance des images Changement de format to-do (utilisation de fonction)

---
+++ src/game/player/character/character/CharacterActionDescription.hpp
@@ -0,0 +1,12 @@
+//
+// Created by devel on 21/10/2023.
+//
+
+#ifndef Z_WAR_CHARACTERACTIONDESCRIPTION_HPP
+#define Z_WAR_CHARACTERACTIONDESCRIPTION_HPP
+
+namespace Game::Player::Chracter::Character{
+
+}
+
+#endif //Z_WAR_CHARACTERACTIONDESCRIPTION_HPP
